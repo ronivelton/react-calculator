e1fc775c7078ec94c6289b8f43de12c9818d7f0e
Ronivelton <roniveltonlemes@gmail.com>
2022-09-16 23:14:26 -0300

    add multiple and division and some improvements in design

--- src/App.jsx
+++ src/App.jsx
@@ -27,21 +27,20 @@ const  initialState = {
 
 function reducer (state, {type, payload}) {
 
+  // Go trough all logic flux of operations
   const makeLogicOperations = (operator) => {
     console.log("here")
 
     if (state.operation === "=") return {currentNumber: "0", previousNumber: state.currentNumber, operation: operator}
 
-    if(state.currentNumber === "0" && (state.previousNumber && state.operation)) return {...state, operation: operator}
+    if (state.previousNumber === null) return {currentNumber: "0", previousNumber: state.currentNumber, operation: operator}
 
-    else if (state.currentNumber === "0") return state
+    if (state.operation === "/" && state.currentNumber === "0") return {...initialState, currentNumber: "Cannot divide by zero"}
 
-    else if (state.previousNumber === null) return {currentNumber: "0", previousNumber: state.currentNumber, operation: operator}
-
-
-    return {currentNumber: "0", previousNumber: `${ makeMathOperations(operator) }`, operation: operator} 
+    return {currentNumber: "0", previousNumber: `${ makeMathOperations(state.operation) }`, operation: operator} 
   }
 
+  // Make the mathematical operations
   const makeMathOperations = (operator) => {
     switch(operator){
       case "+":
@@ -49,6 +48,14 @@ function reducer (state, {type, payload}) {
       
       case "-":
         return Number(state.previousNumber) - Number(state.currentNumber)
+      
+      case "x":
+        return Number(state.previousNumber) * Number(state.currentNumber)
+      
+      case "/":
+        console.log("ola")
+        
+        return Number(state.previousNumber) / Number(state.currentNumber)
     }
   }
 
@@ -57,6 +64,8 @@ function reducer (state, {type, payload}) {
 
     // Add digit
     case ACTIONS_TYPES.addNumber:
+
+      // If the last operation made was the equal operation, then a need clear the screen for nexts digits
       if (state.operation === "=") return {currentNumber: payload.digit, previousNumber: null, operation: null}
 
       if (state.currentNumber.length > 20) return state
@@ -66,17 +75,43 @@ function reducer (state, {type, payload}) {
 
       return {...state, currentNumber: state.currentNumber === "0" ? payload.digit : `${state.currentNumber}${payload.digit}`}
 
+    // Delete a digit
+    case ACTIONS_TYPES.delete:
+      if (state.operation === "=" || state.currentNumber === "Cannot divide by zero") return initialState
+
+      if (state.currentNumber.length === 1) return {...state, currentNumber: "0"}
+
+      return {...state, currentNumber: state.currentNumber.slice(0,-1)}
+
+    // Clear the screen
+    case ACTIONS_TYPES.deleteAll:
+      return initialState
+    
+    // Add dot 
+    case ACTIONS_TYPES.dot:
+      if (state.currentNumber.includes(".")) return state
+
+      return {...state, currentNumber: `${state.currentNumber}${payload.digit}`}
+
     // Sum Operation
     case ACTIONS_TYPES.addOperation:
       return makeLogicOperations("+")
 
     // Subtract Operation
     case ACTIONS_TYPES.subtractOperation:
-      return makeLogicOperations("-")      
+      return makeLogicOperations("-")
+
+    case ACTIONS_TYPES.divisionOperation:
+      return makeLogicOperations("/")
+
+    case ACTIONS_TYPES.multipleOperation:
+      return makeLogicOperations("x")
 
     // Equal Operation
     case ACTIONS_TYPES.equalOperation:
-      if (state.currentNumber === "0" || state.operation === null) return state
+      if(state.operation === "=" || state.operation === null) return state
+
+      if (state.operation === "/" && state.currentNumber === "0") return state
 
       return {currentNumber: `${ makeMathOperations(state.operation) }`, previousNumber: `${state.previousNumber} ${state.operation} ${state.currentNumber}`, operation: "="}
       
@@ -91,9 +126,11 @@ function App() {
 
 
   return (
-    <main className={styles.app}>
-      <div className={styles.calculator}>
-        <div className={styles.screenWrapper}>
+    <>
+      <div className={styles.app}>
+        <main className={styles.calculator}>
+
+          <div className={styles.screenWrapper}>
             <span className={styles.screenPrevious}>
               {previousNumber} {operation}
             </span>
@@ -102,33 +139,37 @@ function App() {
             </span>
           </div>
 
-            <CalculatorButtons type={ACTIONS_TYPES.percentage}>%</CalculatorButtons>
-            <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.deleteAll}>AC</CalculatorButtons>
-            <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.delete}>DEL</CalculatorButtons>
-            <CalculatorButtons type={ACTIONS_TYPES.divisionOperation}>/</CalculatorButtons>
+          <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.deleteAll}>AC</CalculatorButtons>
+          <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.delete}>DEL</CalculatorButtons>
+          <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.divisionOperation}>/</CalculatorButtons>
 
-            <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.addNumber}>7</CalculatorButtons>
-            <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.addNumber}>8</CalculatorButtons>
-            <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.addNumber}>9</CalculatorButtons>
-            <CalculatorButtons type={ACTIONS_TYPES.multipleOperation}>x</CalculatorButtons>
+          <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.addNumber}>7</CalculatorButtons>
+          <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.addNumber}>8</CalculatorButtons>
+          <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.addNumber}>9</CalculatorButtons>
+          <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.multipleOperation}>x</CalculatorButtons>
 
-            <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.addNumber}>4</CalculatorButtons>
-            <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.addNumber}>5</CalculatorButtons>
-            <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.addNumber}>6</CalculatorButtons>
-            <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.subtractOperation}>-</CalculatorButtons>
+          <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.addNumber}>4</CalculatorButtons>
+          <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.addNumber}>5</CalculatorButtons>
+          <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.addNumber}>6</CalculatorButtons>
+          <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.subtractOperation}>-</CalculatorButtons>
 
-            <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.addNumber}>1</CalculatorButtons>
-            <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.addNumber}>2</CalculatorButtons>
-            <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.addNumber}>3</CalculatorButtons>
-            <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.addOperation}>+</CalculatorButtons>
+          <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.addNumber}>1</CalculatorButtons>
+          <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.addNumber}>2</CalculatorButtons>
+          <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.addNumber}>3</CalculatorButtons>
+          <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.addOperation}>+</CalculatorButtons>
 
-            <CalculatorButtons type={ACTIONS_TYPES.sign}>+/-</CalculatorButtons>
-            <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.addNumber}>0</CalculatorButtons>
-            <CalculatorButtons type={ACTIONS_TYPES.dot}>.</CalculatorButtons>
-            <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.equalOperation}>=</CalculatorButtons>
-      
+          <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.sign}>+/-</CalculatorButtons>
+          <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.addNumber}>0</CalculatorButtons>
+          <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.dot}>.</CalculatorButtons>
+          <CalculatorButtons dispatch = {dispatch} type={ACTIONS_TYPES.equalOperation}>=</CalculatorButtons>
+        
+        </main>
+
+        <footer>Made by <a href="https://github.com/ronivelton"><strong>Roni</strong></a></footer>
       </div>
-    </main>
+
+      
+    </>
   )
 }
 
